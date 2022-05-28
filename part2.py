694f7be08afdda8657852cc066d63bb548dd7959
Ella Karhi <100384206+ellakarhi@users.noreply.github.com>
2022-05-28 17:14:19 +0000

    Atm transaction is being worked on

--- part2.py
+++ part2.py
@@ -1,73 +1,96 @@
-# ATM Program
 import os
 
 def menu(id):
-  print ("Press 1 to view account balance")
+  print ("Press 1 to view account balance ")
   print ("Press 2 to withdraw")
   print ("Press 3 to deposit")
-  print ("Press 4 to exit")
+  print ("Press 4 to exit") 
 
   try:
     choice = int(input())
     if choice == 1:
       with open(id + ".txt") as f:
         account = f.readlines()
-      
       if account:
-        print ("Your balance is $" + account[0])
-        menu(id)
+        print(account[0])
       else:
-        print ("Your balance is $0")
-        menu(id)
+        print("Your balance is $0")
+
     elif choice == 2:
       withdraw(id)
       menu(id)
     elif choice == 3:
-      print (1)
+      print(3)
     else:
-      print("Thank you. Goodbye.")
-      return
+      print("Thank you")
+      return 
   except:
     menu(id)
+
 
 def withdraw(id):
   new_balance = 0
   
   with open(id + ".txt") as f:
+    
     balance = f.readlines()
   f.close()
 
   try:
-    amount = int(input("Please enter amount: $"))
+    amount = int(input("Please enter amount "))
   except:
     withdraw(id)
 
   if amount > int(balance[0]):
-    print ("You cannot withdraw more than you have.")
+    print("You cant withdraw more than your balance")
     withdraw(id)
-
+  #else:
+    #new_balance = int(balance[0] -amount)
+  #fwrite = open(id + ".txt", "w")
   int_balance = int(balance[0])
   new_balance = int_balance - amount
                     
   fwrite = open(id + ".txt", "w")
   fwrite.write(str(new_balance))
-  print(f"Your new balance is {new_balance}")
   fwrite.close()
 
+
+  
 def create_account(id):
   open(id + ".txt", "w")
   menu(id)
-
+  
 def start():
   try:
     id = int(input("Please enter your bank ID: "))
     str_id = str(id)
-    if os.path.isfile(str_id + ".txt"):
-      menu(str_id)
+    if os.path.isfile(id + ".txt"):
+      menu(id)
+    
     else:
       create_account(str_id)
+
   except:
     start()
 
 start()
-#menu("9999")
+
+new_line = ""
+""""
+with open("alice.txt") as f:
+  new_line = f.readlines()
+
+f.close()
+
+print (new_line)
+
+new_line[0] = "Hello\n"
+
+print (new_line)
+
+fwrite = open("alice.txt", "w")
+for line in new_line:
+  fwrite.write(line)
+
+fwrite.close()
+"""
